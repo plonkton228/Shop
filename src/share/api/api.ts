a8954d262060757197a1a3a29622729ee472788e
Danik <94543277+plonkton228@users.noreply.github.com>
2023-10-13 00:11:50 +0300

    Update api.ts

--- src/share/api/api.ts
+++ src/share/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios'
 import { USER_LOCALSTORAGE_KEY } from 'share/const/localstorage'
 
-const BaseURL = __API__ || 'http://localhost:8000/'
+const BaseURL = __API__ || 'https://jsonserver-bh4f.onrender.com/'
 console.log(localStorage.getItem(USER_LOCALSTORAGE_KEY))
 // export const api = axios.create({
 //     baseURL: BaseURL,
